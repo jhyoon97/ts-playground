58d8b4f22cb9345c19d484ee6ad3d25ae7b2545c
YoonJihwan <jecgdd@naver.com>
2023-12-03 21:13:01 +0900

    Stack default

--- src/pages/Stack.tsx
+++ src/pages/Stack.tsx
@@ -1,5 +1,59 @@
+import { useState } from "react";
+
+// structures
+import Stack from "structures/Stack";
+
+const stack = new Stack();
+
 const Main = () => {
-  return <div>stack</div>;
+  const [data, setData] = useState(stack.getItems());
+  const [input, setInput] = useState("");
+
+  const handlePush = () => {
+    stack.push(input);
+    setData(stack.getItems());
+    setInput("");
+  };
+
+  const handlePop = () => {
+    stack.pop();
+    setData(stack.getItems());
+  };
+
+  const handlePeak = () => {
+    console.log(stack.peek());
+  };
+
+  const handleClear = () => {
+    stack.clear();
+    setData(stack.getItems());
+  };
+
+  return (
+    <div>
+      {data.map((item) => (
+        <div>{item}</div>
+      ))}
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handlePush();
+        }}
+      >
+        <input onChange={(e) => setInput(e.target.value)} value={input} />
+        <button type="submit">PUSH</button>
+        <button type="button" onClick={handlePop}>
+          POP
+        </button>
+        <button type="button" onClick={handlePeak}>
+          PEAK
+        </button>
+        <button type="button" onClick={handleClear}>
+          CLEAR
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/structures/Stack.ts
@@ -0,0 +1,31 @@
+export default class Stack {
+  items: Array<any> = [];
+
+  push(element: any) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  clear() {
+    this.items = [];
+  }
+
+  size() {
+    return this.items.length;
+  }
+
+  getItems() {
+    return [...this.items];
+  }
+}
