d1586f8042c4f4d2f316f8b6fce7dc1426fc6a2c
YoonJihwan <jecgdd@naver.com>
2023-12-09 16:44:14 +0900

    HashTableWithLinkedList test

--- src/pages/HashTableWithLinkedList.tsx
+++ src/pages/HashTableWithLinkedList.tsx
@@ -12,15 +12,20 @@ const box = css`
 `;
 
 const table = new HashTableWithLinkedList();
-table.set("Gandalf", "gandalf@email.com");
-table.set("John", "john@email.com");
-table.set("Tyrion", "tyrion@email.com");
+/* table.set("Gandalf", "간달프");
+table.set("John", "존");
+table.set("Tyrion", "티리온");
+table.set("Aaron", "아론");
+table.set("Donnie", "도니");
+table.set("Ana", "애나");
+table.set("Jonathan", "조나단");
+table.set("Jamie", "제이미");
+table.set("Sue", "슈");
+table.set("Mindy", "민디");
+table.set("Paul", "폴");
+table.set("Nathan", "나단");
 
-table.set("asd", "collision!!!!!!!!");
-
-table.remove("asd");
-
-console.log(table.getTable());
+console.log(table.getTable()); */
 
 const HashTableWithLinkedListPage = () => {
   return <div css={box} />;
