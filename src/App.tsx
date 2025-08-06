ce2da708e843b395d89621e79467ca1334e00bd6
jhyoon97 <jecgdd@naver.com>
2025-08-06 16:37:20 +0900

    tree print

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,76 @@
 import { useEffect, useState } from "react";
 
-import { BinarySearchTree } from "structures/BinarySearchTree";
+import BinarySearchTree from "structures/BinarySearchTree";
+
+import "./global.scss";
 
 const App = () => {
   const [logs, setLogs] = useState<any[]>([]);
 
-  const log = (newLog: any) => {
-    console.log(newLog);
-    setLogs((prev) => prev.concat(newLog));
+  const log = (...args: any[]) => {
+    setLogs((prev) =>
+      prev.concat(
+        args
+          .map((item) =>
+            typeof item === "object" ? JSON.stringify(item) : item
+          )
+          .join(" ")
+      )
+    );
   };
 
   useEffect(() => {
-    const bst = new BinarySearchTree(50);
+    setLogs([]);
 
+    window.console.log = (...args: any[]) => {
+      log(...args);
+    };
+  }, []);
+
+  useEffect(() => {
+    const bst = new BinarySearchTree(50);
     bst.add(25);
+    bst.add(6);
+    bst.add(41);
+    bst.add(75);
     bst.add(12);
     bst.add(37);
-    bst.add(75);
-    bst.add(63);
+    bst.add(46);
+    bst.add(62);
     bst.add(87);
+    bst.add(3);
+    bst.add(56);
+    bst.add(69);
+    bst.add(80);
+    bst.add(93);
+    bst.print();
 
-    bst.postorder((vertex) => log(vertex.id));
+    const bst2 = new BinarySearchTree(50);
+    bst2.add(25);
+    bst2.add(6);
+    bst2.add(41);
+    bst2.add(75);
+    bst2.add(12);
+    bst2.add(37);
+    bst2.add(46);
+    bst2.add(62);
+    bst2.add(87);
+    bst2.add(3);
+    bst2.add(56);
+    bst2.add(69);
+    bst2.add(80);
+    bst2.add(93);
+    bst2.remove(50);
+    bst2.remove(75);
+    bst2.print();
   }, []);
 
   return (
-    <div>
+    <div style={{ width: "fit-content" }}>
       {logs.map((item, index) => (
-        <p key={index}>{item}</p>
+        <p key={index} style={{ whiteSpace: "pre" }}>
+          {item}
+        </p>
       ))}
     </div>
   );
--- src/structures/BinarySearchTree.ts
+++ src/structures/BinarySearchTree.ts
@@ -11,35 +11,11 @@ class Vertex {
     this.right = null;
   }
 }
-export class BinarySearchTree {
+export default class BinarySearchTree {
   root: Vertex;
 
   constructor(id: number) {
     this.root = new Vertex(id);
-  }
-
-  add(newId: number, vertex?: Vertex) {
-    let target = null;
-
-    if (!vertex) {
-      target = this.root;
-    } else {
-      target = vertex;
-    }
-
-    if (newId < target.id) {
-      if (target.left) {
-        this.add(newId, target.left);
-      } else {
-        target.left = new Vertex(newId);
-      }
-    } else if (newId > target.id) {
-      if (target.right) {
-        this.add(newId, target.right);
-      } else {
-        target.right = new Vertex(newId);
-      }
-    }
   }
 
   has(id: number) {
@@ -58,6 +34,91 @@ export class BinarySearchTree {
     }
 
     return false;
+  }
+
+  add(id: number) {
+    const _add = (newId: number, vertex: Vertex) => {
+      if (newId < vertex.id) {
+        if (vertex.left) {
+          _add(newId, vertex.left);
+        } else {
+          vertex.left = new Vertex(newId);
+        }
+      } else if (newId > vertex.id) {
+        if (vertex.right) {
+          _add(newId, vertex.right);
+        } else {
+          vertex.right = new Vertex(newId);
+        }
+      }
+    };
+
+    if (!this.has(id)) {
+      _add(id, this.root);
+    }
+  }
+
+  getMax(vertex?: Vertex) {
+    let target = vertex ?? this.root;
+
+    while (target.right) {
+      target = target.right;
+    }
+
+    return target;
+  }
+
+  getMin(vertex?: Vertex) {
+    let target = vertex ?? this.root;
+
+    while (target.left) {
+      target = target.left;
+    }
+
+    return target;
+  }
+
+  remove(id: number) {
+    const _remove = (vertex: Vertex, targetId: number) => {
+      if (targetId < vertex.id) {
+        if (vertex.left) {
+          vertex.left = _remove(vertex.left, targetId);
+        }
+      } else if (targetId > vertex.id) {
+        if (vertex.right) {
+          vertex.right = _remove(vertex.right, targetId);
+        }
+      } else {
+        if (vertex.right && vertex.left) {
+          const newVertex = this.getMin(vertex.right);
+          _remove(vertex.right, newVertex.id);
+          newVertex.right = vertex.right;
+          newVertex.left = vertex.left;
+
+          if (this.root.id === targetId) {
+            this.root = newVertex;
+          }
+
+          return newVertex;
+        }
+
+        if (vertex.right) {
+          return vertex.right;
+        }
+
+        if (vertex.left) {
+          return vertex.left;
+        }
+
+        return null;
+      }
+
+      return vertex;
+    };
+
+    if (this.has(id)) {
+      _remove(this.root, id);
+    }
   }
 
   bfs(callback: (vertex: Vertex, level?: number) => void) {
@@ -88,6 +149,53 @@ export class BinarySearchTree {
     }
   }
 
+  printVertexId(id?: number | null) {
+    // id를 5자리 문자열로 변환, 빈 부분은 x로 채우고 id는 최대한 가운데로 오도록
+    const idStr = id ? String(id) : " ";
+    const totalLength = 3;
+    const padLength = totalLength - idStr.length;
+    const leftPad = Math.floor(padLength / 2);
+    const rightPad = padLength - leftPad;
+    return " ".repeat(leftPad) + idStr + " ".repeat(rightPad);
+  }
+
+  print() {
+    let queueStartIndex = 0;
+    const queue: Array<[Vertex | null, number]> = [[this.root, 0]];
+
+    while (queueStartIndex < queue.length) {
+      const [currentVertex, currentLevel] = queue[queueStartIndex++];
+
+      if (currentVertex === null) {
+        continue;
+      }
+
+      queue.push([currentVertex?.left || null, currentLevel + 1]);
+      queue.push([currentVertex?.right || null, currentLevel + 1]);
+    }
+
+    const maxLevel = queue[queue.length - 1][1];
+    for (let i = 0; i <= maxLevel; i++) {
+      const sameLevel = queue.filter(([, level]) => level === i);
+
+      // 각 레벨의 노드들 사이 간격을 계산
+      const nodeSpacing = Math.pow(2, maxLevel - i + 1) * 3;
+      // 첫 번째 노드의 왼쪽 여백
+      const leftPadding = Math.pow(2, maxLevel - i) * 3 - 2;
+
+      const line = sameLevel
+        .map(([vertex], index) => {
+          const nodeText = this.printVertexId(vertex?.id);
+          return index === 0
+            ? nodeText
+            : " ".repeat(nodeSpacing - 3) + nodeText;
+        })
+        .join("");
+
+      console.log(" ".repeat(leftPadding) + line);
+    }
+  }
+
   preorder(callback: (vertex: Vertex) => void) {
     const traverse = (
       _callback: (vertex: Vertex) => void,
@@ -101,7 +209,7 @@ export class BinarySearchTree {
     traverse(callback, this.root);
   }
 
-  inorder(callback: (vertex: Vertex) => void, node?: Vertex) {
+  inorder(callback: (vertex: Vertex) => void) {
     const traverse = (
       _callback: (vertex: Vertex) => void,
       vertex: Vertex | null
@@ -114,7 +222,7 @@ export class BinarySearchTree {
     traverse(callback, this.root);
   }
 
-  postorder(callback: (vertex: Vertex) => void, node?: Vertex) {
+  postorder(callback: (vertex: Vertex) => void) {
     const traverse = (
       _callback: (vertex: Vertex) => void,
       vertex: Vertex | null
